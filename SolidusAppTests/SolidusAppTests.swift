0c3660e5c15fdca77a9142527673ab31a83b49a0
Race Young <racejyoung@gmail.com>
2025-11-18 02:16:13 -0700

    Initial Commit

---
+++ SolidusAppTests/SolidusAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SolidusAppTests.swift
+//  SolidusAppTests
+//
+//  Created by racecar on 11/18/25.
+//
+
+import Testing
+@testable import SolidusApp
+
+struct SolidusAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
